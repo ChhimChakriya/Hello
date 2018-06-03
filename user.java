39ee20583bdb4f313cb350e352018a3a41d1616c
ChhimChakriya <chakriyachhim.sr@gmail.com>
2018-06-04 03:51:19 +0700

    Fist Commit

---
+++ user.java
@@ -0,0 +1,11 @@
+class User {
+	private  int id;
+	private String name;
+	private String  gender;
+
+	public User(int id ,String name,String gender){
+	this.id = id;
+	this.name = name;
+	this.gender = gender;	
+}
+}
